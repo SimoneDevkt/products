6ba0de3e5163e60da53b1f17389710f79b69c5b1
Simone <simonep97@gmail.com>
2023-05-21 19:07:46 +0200

    add tests

--- test/routes/products.test.ts
+++ test/routes/products.test.ts
@@ -25,12 +25,20 @@ test('200 /products/raw/:id', async (t) => {
   }, 'check response format');
   t.equal(res.statusCode, 200, 'returns a status code of 200')
 })
-
-test('404 /products/raw/:id', async (t) => {
+test('400 /products/raw/:id', async (t) => {
   const app = await build(t)
 
   const res = await app.inject({
     url: '/products/raw/sdf'
+  })
+
+  t.equal(res.statusCode, 400, 'returns a status code of 400')
+})
+test('404 /products/raw/:id', async (t) => {
+  const app = await build(t)
+
+  const res = await app.inject({
+    url: '/products/raw/122'
   })
 
   t.equal(res.statusCode, 404, 'returns a status code of 404')
@@ -55,12 +63,20 @@ test('/products/proc/:id', async (t) => {
   }, 'check response format');
   t.equal(res.statusCode, 200, 'returns a status code of 200')
 })
+test('400 /products/proc/:id', async (t) => {
+  const app = await build(t)
 
+  const res = await app.inject({
+    url: '/products/proc/asdf'
+  })
+
+  t.equal(res.statusCode, 400, 'returns a status code of 400')
+})
 test('404 /products/proc/:id', async (t) => {
   const app = await build(t)
 
   const res = await app.inject({
-    url: '/products/proc/sdf'
+    url: '/products/proc/122'
   })
 
   t.equal(res.statusCode, 404, 'returns a status code of 404')
@@ -81,12 +97,20 @@ test('/products/download/:id', async (t) => {
   }, 'check response format');
   t.equal(res.statusCode, 200, 'returns a status code of 200')
 })
-
-test('404 /products/download/:id', async (t) => {
+test('400 /products/download/:id', async (t) => {
   const app = await build(t)
 
   const res = await app.inject({
     url: '/products/download/sdf'
+  })
+
+  t.equal(res.statusCode, 400, 'returns a status code of 400')
+})
+test('404 /products/download/:id', async (t) => {
+  const app = await build(t)
+
+  const res = await app.inject({
+    url: '/products/download/122'
   })
 
   t.equal(res.statusCode, 404, 'returns a status code of 404')
