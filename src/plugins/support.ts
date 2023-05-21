e22fc755d2d3af9d2ca40917e2da459b084e23c3
Simone <simonep97@gmail.com>
2023-05-21 18:45:05 +0200

    add util functions

--- src/plugins/support.ts
+++ src/plugins/support.ts
@@ -7,14 +7,34 @@ export interface SupportPluginOptions {
 // The use of fastify-plugin is required to be able
 // to export the decorators to the outer scope
 export default fp<SupportPluginOptions>(async (fastify, opts) => {
-  fastify.decorate('someSupport', function () {
-    return 'hugs'
+  fastify.decorate('roundedDiscount', function (price: number, discountPercentage: number) : number{
+    function roundPrice(number: number): number {
+      const rounded = Math.round(number * 100) / 100;
+      return +rounded.toFixed(2);
+    }
+    function getDiscount(price: number, discountPercentage: number): number{
+      return price * ((100 - discountPercentage)/100)
+    }
+    return roundPrice(getDiscount(price, discountPercentage))
   })
+
+  fastify.decorate('getBase64FromUrl', async function (url: string) : Promise<string>{
+    let imageUrlData = await fetch(url)
+    const buffer = await imageUrlData.arrayBuffer();
+    const stringifiedBuffer = Buffer.from(buffer).toString('base64');
+    const contentType = imageUrlData.headers.get('content-type');
+    return `data:${contentType};base64,${stringifiedBuffer}`;    
+  })
+
+  
 })
+
+
 
 // When using .decorate you have to specify added properties for Typescript
 declare module 'fastify' {
   export interface FastifyInstance {
-    someSupport(): string;
+    roundedDiscount(price: number, discountPercentage: number): Function;
+    getBase64FromUrl(url: string) :Promise<string>;
   }
 }
